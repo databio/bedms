8ab38ca5ac29774eabd97cd4bb38339bc9493a60
saanikat <saanikatambe@gmail.com>
2024-04-03 09:56:26 -0400

    optim changes

--- model1/nn_model1_optim.py
+++ model1/nn_model1_optim.py
@@ -6,6 +6,7 @@ from nn_model1_train import ModelTraining
 import pickle
 
 param_grid={
+    'batch_size':[32, 64, 128],
     'hidden_size':[32,64,128],
     'learning_rate':[0.001,0.005,0.01]
 }
@@ -22,7 +23,7 @@ best_val_losses=None
 for params in param_combinations:
     print("Running on hyperparameters:", params)
     trainer=ModelTraining(input_size, params['hidden_size'], output_size, learning_rate=params['learning_rate'])
-    train_accuracies, val_accuracies, train_losses, val_losses=trainer.train(X_train_tensor, y_train_tensor,X_val_tensor, y_val_tensor, num_epochs=10, batch_size=32, device="cpu")
+    train_accuracies, val_accuracies, train_losses, val_losses=trainer.train(X_train_tensor, y_train_tensor,X_val_tensor, y_val_tensor, num_epochs=10, batch_size=params['batch_size'], device="cpu")
     current_val_accuracy=val_accuracies[-1]
     print("Validation Accuracy:", current_val_accuracy)
     if current_val_accuracy > best_accuracy:
@@ -40,8 +41,9 @@ print("Best Validation Accuracy:", best_accuracy)
 best_model_path="nn_model1_best.pth"
 torch.save(best_model.state_dict(), best_model_path)
 
-results={'best_train_accuracies': best_train_accuracies, 'best_val_accuracies':best_val_accuracies,
+results={'best_hyperparameters': best_params, 'best_train_accuracies': best_train_accuracies, 'best_val_accuracies':best_val_accuracies,
          'best_train_losses':best_train_losses, 'best_val_losses':best_val_losses}
+
 with open('hyperparam_optim_results.pkl', 'wb') as f:
     pickle.dump(results, f)
     
