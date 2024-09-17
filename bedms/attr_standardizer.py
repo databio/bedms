50969451a450643de1445ed086b5d24ab8c100e9
saanikat <saanikatambe@gmail.com>
2024-09-17 09:07:15 -0400

    updated readme

--- bedms/attr_standardizer.py
+++ bedms/attr_standardizer.py
@@ -248,7 +248,7 @@ class AttrStandardizer:
             )
 
     @staticmethod
-    def get_available_schemas() -> list[str]:
+    def show_available_schemas() -> list[str]:
         """
         Stores a list of available schemas.
 
